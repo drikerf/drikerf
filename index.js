7dbfb4fd1eb2dd25b78042dee2d49f5e6575fc10
Fredrik <Fredrik@Fredriks-MacBook-Air.local>
2014-11-06 21:10:15 +0100

    new index, projects page

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 var express = require('express');
 var app = express();
 
+// Port.
 app.set('port', (process.env.PORT || 3000));
+
+// Views.
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+
+// Static files.
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
-})
+// Routes.
+app.get('/', function(req, res) {
+	res.render('index');
+});
 
+app.get('/projects', function(req, res) {
+	res.render('projects');
+});
+
+app.get('/secret', function(req, res) {
+  res.send('You found it!');
+});
+
+// Start app.
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'));
-})
+  console.log("Running on:" + app.get('port'));
+});
